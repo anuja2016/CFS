5b70f04c95549f96dea7895e0d712a0378d583f1
nikitagcoreflex <145319153+nikitagcoreflex@users.noreply.github.com>
2023-09-20 17:54:03 +0530

    home page changes updated

--- index.php
+++ index.php
@@ -2,33 +2,235 @@
 include_once 'header.php';
 ?>
 
-
 <div class="carousel slide carousel-fade main-carousel" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="assets/images/slider1.svg" class="d-block w-100 img-responsive" alt="..." /> 
-      <div class="carousel-caption d-none d-md-block">
-      <h1>One <span>Platform </span>
-        <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/images/slider1.svg" class="d-block w-100 img-responsive" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+                <h1>One <span>Platform </span>
+                    <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service
+                        Businesses.</p>
 
-      </div>
-       <!-- <div>
+            </div>
+            <!-- <div>
       <h1>One <span>Platform </span>
       For All Field Service</h1>
       <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
       </div>  -->
+        </div>
+        <div class="carousel-item">
+            <img src="assets/images/slider2.svg" class="d-block w-100 img-responsive" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/images/slider3.svg" class="d-block w-100 img-responsive" alt="...">
+        </div>
     </div>
-    <div class="carousel-item">
-      <img  src="assets/images/slider2.svg" class="d-block w-100 img-responsive" alt="..." >
+
+</div>
+
+<div class="text-center mt-5 mb-3">
+    <p class="title-red">OUR POWERFUL FEATURES</p>
+    <p class="title">Simplify Your Service Business Operations</p>
+</div>
+<div class="row m-4">
+    <div class="col-md-3 col-sm-12 text-center">
+        <img src="assets/images/createsquotus.svg" />
+        <p class="title-sm mt-2 mb-2">Create Quotes</p>
+        <p class="pt-1 title-sm-para">Generate accurate quotes instantly, tailored to client needs. Simplify the quoting
+            process effortlessly with different flexibility of putting Discount, SLA and Tax.</p>
+
     </div>
-    <div class="carousel-item">
-      <img  src="assets/images/slider3.svg" class="d-block w-100 img-responsive" alt="..." >
+    <div class="col-md-3 col-sm-12 text-center ">
+        <img src="assets/images/intelligenticon.svg" />
+        <p class="title-sm mt-2 mb-2">Intelligent Scheduling</p>
+        <p class="pt-1 title-sm-para">Effortlessly Schedule task to your Field worker with respect to their work
+            speciality, allocated work
+            area, time availability and nearest location.</p>
     </div>
-  </div>
- 
+    <div class="col-md-3 col-sm-12 text-center">
+        <img src="assets/images/smartdispatching.svg" />
+        <p class="title-sm mt-2 mb-2">Smart Dispatching</p>
+        <p class="pt-1 title-sm-para">Utilize cutting-edge technology to efficiently track your workforce, ensuring
+            automatic assignment of the most suitable technician to each job.
+        </p>
+    </div>
+    <div class="col-md-3 col-sm-12 text-center">
+        <img src="assets/images/effertlessicon.svg" />
+        <p class="title-sm mt-2 mb-2">Effortless Invoicing</p>
+        <p class="pt-1 title-sm-para">Streamline invoicing process with just one click. Enjoy digital invoicing,
+            automated payment reminders, comprehensive record of full, partial, unpaid statuses.
+        </p>
+    </div>
+
+</div>
+
+<div class="text-center mt-5 mb-3">
+    <p class="title-red">FEATURES FOR YOURS</p>
+    <p class="title">Home Service Business</p>
+</div>
+<div class="container-fluid mb-3">
+    <div class="row">
+        <div class="col-md-5">
+            <div class="block-light">
+                <p class="title">Streamline Your Business Operations</p>
+                <p class="title-lg-para pt-2">Say goodbye to juggling multiple platforms. Our all-in-one Field Service
+                    Management solution centralizes everything you need to efficiently run your Field service business
+                    in one convenient location, accessible wherever you are.
+                </p>
+
+                <button type="button" class="btn-outline-red d-flex mt-5">
+                    <p>Learn More
+                    <p><span class="ms-2"><img src="assets/images/whitearrow.svg" class="signin-size" /></span>
+                </button>
+            </div>
+
+        </div>
+        <div class="col-md-7 mt-3">
+          
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/jobschedulling.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Job
+                                Scheduling & Dispatching</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/invoice.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Quotation/Invoice</p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/inventory.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Inventory Management</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/asset.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Asset
+Management</p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/customerapp.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Customer 
+App</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/amcicon.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">AMC
+Management</p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/realtracking.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Real Time 
+Tracking</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/jobreport.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md"> Job Report</p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/leadicon.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Lead Management</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="d-flex align-items-center gap-3">
+                        <div class="flex-1">
+                            <img src="assets/images/serviceicon.svg" />
+                        </div>
+                        <div class="flex-1">
+                            <p class="title-md">Service Management</p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </div>
 
 
+<div class="text-center mt-5 mb-3">
+    <p class="title-red">50+ INDUSTRIES</p>
+    <p class="title">Served by FieldWeb</p>
+</div>
 <?php
 include_once 'footer.php';
 ?>
