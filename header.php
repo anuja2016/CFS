b2a84b41f6c0c6e940006705de9fb14b07ffedb0
Mahesh Jagtap <maheshj@coreflexsolutions.com>
2023-09-21 18:20:51 +0530

    fixes the home page related ui

--- header.php
+++ header.php
@@ -7,83 +7,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='assets/css/style.css' rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-
-        <script src=
-"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity=
-"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous">
-    </script>
-        
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <title>FieldWeb</title>
-   
 </head>
 
 <body>
-
-<div class="main-header-navbar fixed-top ">
-     <div class="header-top container-fluid">
-        <ul class="me-2">
-            <li>+91 93152 28028</li>
-            <li>|</li>
-            <li><a href="#" class="d-flex">Sign in <span class="ms-2"><img src="assets/images/signin.svg" class="signin-size" /></span> </a></li>
-        </ul>
-    </div> 
-     <nav class="navbar navbar-expand-lg navbar-light bg-white">
-       
-        <div class="container-fluid">
-
-           
-
+    <div class="site-header fixed-top ">
+        <div class="header-top container-fluid">
+            <ul class="me-2">
+                <li><a href="tel:+919315228028">+91 93152 28028</a></li>
+                <li>|</li>
+                <li><a href="#" class="d-flex">Sign in <span class="ms-2"><img src="assets/images/signin.svg" class="signin-size" /></span> </a></li>
+            </ul>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="#"> <img src="assets/images/logo.svg" /></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-           
-
-            <div class="collapse navbar-collapse justify-content-end" id="navbarText">
-                <ul class="navbar-nav  mb-2 mb-lg-0">
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link" href="#">Industries</a>
-                    </li>
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link" href="#">Blog</a>
-                    </li>
-                    <li class="nav-item  navbar-menu-title">
-                        <a class="nav-link" href="#">Contact Us</a>
-                    </li>
-                </ul>
-
-                <div class="navbar-text">
-                <button type="button" class="btn-outline-black">Explore Pricing</button>
-                <button type="button" class="btn-outline-red">Get Demo</button>
-               </div>
-
+                <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+                    <ul class="navbar-nav  mb-2 mb-lg-0">
+                        <li>
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Industries</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                    <div class="navbar-buttons">
+                        <button type="button" class="btn-outline-black">Explore Pricing</button>
+                        <button type="button" class="btn-red">Get Demo</button>
+                    </div>
+                </div>
             </div>
-            
-        </div>
-
-    </nav> 
-</div>
-
-
-
-
+        </nav>
+    </div>
+    <!--scripts--->
+    <script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -6,26 +6,66 @@ include_once 'header.php';
 <div class="carousel slide carousel-fade main-carousel" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="assets/images/slider1.svg" class="d-block w-100 img-responsive" alt="..." /> 
-      <div class="carousel-caption d-none d-md-block">
-      <h1>One <span>Platform </span>
+      <img src="assets/images/slider1.svg" class="img-fluid" alt="..." />
+      <div class="carousel-caption d-md-block">
+        <h1>One <span>Platform</span> For All Field Service</h1>
         <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
-
+        <div class="sect-getservice">
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">
+              <select class="form-control">
+                <option>India</option>
+                <option>Albania</option>
+                <option>Algeria</option>
+              </select>
+            </span>
+            <input type="text" class="form-control" placeholder="Enter Your mobile number " aria-label="MobileNumber">
+          </div>
+          <button type="submit" class="btn-getstarted">Get Started</button>
+        </div>
       </div>
-       <!-- <div>
-      <h1>One <span>Platform </span>
-      For All Field Service</h1>
-      <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
-      </div>  -->
     </div>
     <div class="carousel-item">
-      <img  src="assets/images/slider2.svg" class="d-block w-100 img-responsive" alt="..." >
+      <img src="assets/images/slider2.svg" class="img-fluid" alt="...">
+      <div class="carousel-caption d-md-block">
+        <h1>One <span>Platform</span> For All Field Service</h1>
+        <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
+        <div class="sect-getservice">
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">
+              <select class="form-control">
+                <option>India</option>
+                <option>Albania</option>
+                <option>Algeria</option>
+              </select>
+            </span>
+            <input type="text" class="form-control" placeholder="Enter Your mobile number " aria-label="MobileNumber">
+          </div>
+          <button type="submit" class="btn-getstarted">Get Started</button>
+        </div>
+      </div>
     </div>
     <div class="carousel-item">
-      <img  src="assets/images/slider3.svg" class="d-block w-100 img-responsive" alt="..." >
+      <img src="assets/images/slider3.svg" class="img-fluid" alt="...">
+      <div class="carousel-caption d-md-block">
+        <h1>One <span>Platform</span> For All Field Service</h1>
+        <p>Welcome to FieldWeb: Streamline Your Field Service Management. A Powerful Solution for Service Businesses.</p>
+        <div class="sect-getservice">
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">
+              <select class="form-control">
+                <option>India</option>
+                <option>Albania</option>
+                <option>Algeria</option>
+              </select>
+            </span>
+            <input type="text" class="form-control" placeholder="Enter Your mobile number " aria-label="MobileNumber">
+          </div>
+          <button type="submit" class="btn-getstarted">Get Started</button>
+        </div>
+      </div>
     </div>
   </div>
- 
 </div>
 
 
